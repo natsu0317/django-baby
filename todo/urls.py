4abd9b70b3c4fcb9d52d4e489442b4c351d8a66b
natsu <natsuko031725@icloud.com>
2022-12-10 14:46:55 +0900

    c

--- todo/urls.py
+++ todo/urls.py
@@ -4,7 +4,7 @@ from .views import todo_add, todo_delete,todo_list,todo_add_view,todo_delete_vie
 app_name = 'todo'
 
 urlpatterns = [
-    path('/',todo_list), #/todoにするとtodo_listに飛ぶ
+    path('',todo_list), #/todoにするとtodo_listに飛ぶ
     path('add/', todo_add),
     path('delete/<int:id>', todo_delete),
     path('todo-add/',todo_add_view.as_view()),
